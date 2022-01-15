0e5ac1f58e2cb7fdfc2751cfed1efbee3a5b01c4
Philippe Clesca <hello@philippec.me>
2022-01-14 19:16:31 -0500

    started work on react plugin

---
+++ packages/plexus-react/src/index.ts
@@ -0,0 +1,1 @@
+export {usePlexus} from './usePlexus'
---
+++ packages/plexus-react/src/usePlexus.ts
@@ -0,0 +1,8 @@
+import {useEffect, useState} from 'react';
+
+export function usePlexus(){
+	const [] = useState()
+	useEffect(() => {
+
+	}, [])
+} 
