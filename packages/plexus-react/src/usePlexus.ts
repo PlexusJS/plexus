34442f288f68e019736333d71d019f8199e92b46
Philippe Clesca <hello@philippec.me>
2022-01-15 04:29:56 -0500

    started on react hook for plexus state

--- packages/plexus-react/src/usePlexus.ts
+++ packages/plexus-react/src/usePlexus.ts
@@ -1,8 +1,28 @@
+import {state} from '@plexusjs/core'
+// import {  } from '@plexusjs/core/dist/interfaces';
 import {useEffect, useState} from 'react';
 
-export function usePlexus(){
-	const [] = useState()
+export function usePlexus<State extends ReturnType<typeof state>>(deps: State)
+export function usePlexus<State extends ReturnType<typeof state>>(deps: State[])
+export function usePlexus<State extends ReturnType<typeof state>>(deps: State | State[]){
+	const [_, set] = useState(null)
 	useEffect(() => {
+		if(Array.isArray(deps)){
+			const depSubIds: Map<string|number, State> = new Map()
+			for(let dep of deps){
+				
+				const id = dep.watch(set)
+				depSubIds.set(id, dep)
+				
+			}
+			return () => {
+				for(let unsub of depSubIds){
+					const [id, value] = unsub
+					value.removeWatcher(id)
+				}
+			}
+		}
 
 	}, [])
+
 } 
