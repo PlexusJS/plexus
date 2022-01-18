70d44538a2ae85712ad700473d463d73a14c6d64
Philippe Clesca <hello@philippec.me>
2022-01-18 00:56:08 -0500

    improved collection group

--- packages/plexus-core/src/collection/group.ts
+++ packages/plexus-core/src/collection/group.ts
@@ -1,13 +1,22 @@
 import { PlexusCollectionInstance } from "..";
+import { PlexusInstance } from "../instance";
 
 export interface PlexusCollectionGroupConfig<DataType> {
 	addWhen?: (item: DataType) => boolean
 }
-export type PlexusCollectionGroup = ReturnType<typeof group>
-export function group<DataType=any>(name: string, config?: PlexusCollectionGroupConfig<DataType>){
+export interface PlexusCollectionGroup<DataType> {
+	has( key: string | number ): boolean,
+	add( key: string | number ): PlexusCollectionGroup<DataType>,
+	remove( key: string | number ): PlexusCollectionGroup<DataType>,
+	get includedKeys(): Set<string | number>
+	get value(): DataType[]
+
+}
+export function _group<DataType=any>(instance: () => PlexusInstance, collectionId: string, name: string, config?: PlexusCollectionGroupConfig<DataType>){
 	const _internalStore = {
 		addWhen: config?.addWhen || (() => false),
 		_name: name,
+		_collectionId: collectionId,
 		_includedKeys: new Set<string|number>()
 	}
 
@@ -17,12 +26,17 @@ export function group<DataType=any>(name: string, config?: PlexusCollectionGroup
 		},
 		add( key: string | number ){
 			_internalStore._includedKeys.add(key)
+			return this as PlexusCollectionGroup<DataType>
 		},
 		remove( key: string | number ){
 			_internalStore._includedKeys.delete(key)
+			return this as PlexusCollectionGroup<DataType>
 		},
 		get includedKeys(){
 			return _internalStore._includedKeys
+		},
+		get value(){
+			return instance()._collections.get(_internalStore._collectionId).groupsValue[_internalStore._name]
 		}
 	}
 }
