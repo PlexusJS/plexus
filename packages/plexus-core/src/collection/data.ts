436aba39c57bbaa9b21868d263543e5e045522f4
Philippe Clesca <hello@philippec.me>
2022-01-18 00:56:22 -0500

    improved collection data

--- packages/plexus-core/src/collection/data.ts
+++ packages/plexus-core/src/collection/data.ts
@@ -2,7 +2,7 @@ import { PlexusStateInstance, state } from "..";
 
 export interface PlexusDataInstance<TypeValue> {
 	get value(): TypeValue
-	set(value: TypeValue): void	
+	set(value: TypeValue, config?: {mode: 'set' | 'patch'}): void	
 	get state(): PlexusStateInstance<TypeValue>
 }
 
@@ -18,10 +18,19 @@ export function _data<Value extends Record<string, any>>(primaryKey: string, val
 			get value(){
 				return _internalStore._state.value
 			},
-			set: (value: Value) => {
-				if(value[_internalStore.primaryKey] !== undefined && value[_internalStore.primaryKey] === _internalStore._key){
-					_internalStore._state.set(value)
+			set: (value: Partial<Value>, config: {mode: 'set' | 'patch'}={mode: 'set'}) => {
+				const checkIfHasKey = () => value[_internalStore.primaryKey] !== undefined && value[_internalStore.primaryKey] === _internalStore._key
+				if(config.mode === 'set'){
+					if(checkIfHasKey()){
+						_internalStore._state.set(value as Value)
+					}
 				}
+				else{
+					if(value[_internalStore.primaryKey] === _internalStore._key){
+						_internalStore._state.patch(value as Value)
+					}
+				}
+				
 			},
 			get state(){
 				return _internalStore._state
