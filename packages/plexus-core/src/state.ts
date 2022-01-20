96cf791dfe80e032cdaf463ac6110731cc2177c5
Philippe Clesca <hello@philippec.me>
2022-01-20 11:29:14 -0500

    Alight improvments to state and it's file structure

--- packages/plexus-core/src/state.ts
+++ packages/plexus-core/src/state.ts
@@ -34,10 +34,10 @@ export interface PlexStateInternalStore<Value> {
 	externalName: string
 }
 
-export function _state<PxStateValue extends PlexusStateType>(instance: () => PlexusInstance, _init: PxStateValue) {
+export function _state<StateValue extends PlexusStateType>(instance: () => PlexusInstance, _init: StateValue) {
 
 	// props // 
-	const _internalStore: PlexStateInternalStore<PxStateValue> = {
+	const _internalStore: PlexStateInternalStore<StateValue> = {
 		_nextValue: null,
 		_value: _init,
 		_initialValue: _init,
@@ -50,164 +50,125 @@ export function _state<PxStateValue extends PlexusStateType>(instance: () => Ple
 	}
 	
 	// Methods //
-	/**
-	 * Set the value of the state
-	 * @param value 
-	*/
-	function set(value: PxStateValue) {
-
-		_internalStore._lastValue = _internalStore._value
-		if(isObject(value) && isObject(_internalStore._value)) {
-			_internalStore._lastValue = deepClone(_internalStore._value)
-		}
-		else if(Array.isArray(value) && Array.isArray(_internalStore._value)) {
-			const obj = deepMerge(_internalStore._value, value)
-			_internalStore._lastValue = (Object.values(obj) as PxStateValue)
-		}
-		else{
-			_internalStore._lastValue = _internalStore._value
-		}
-		_internalStore._value = value
-		_internalStore._nextValue = value
-
-		// update the runtime conductor
-		instance()._runtime.stateChange(_internalStore._name, value)
-		if(_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
-	}
-	/**
-	 * Patch the current value of the state
-	 * @param value A value of the state to merge with the current value
-	*/
-	function patch(value: PxStateValue) {
-		
-		if(isObject(value) && isObject(_internalStore._value)) {
-			set(deepMerge(_internalStore._value, value))
-		}
-		// if the deep merge is on an array type, we need to convert the merged objecct back to an array
-		else if(Array.isArray(value) && Array.isArray(_internalStore._value)) {
-			const obj = deepMerge(_internalStore._value, value)
-			set(Object.values(obj) as PxStateValue)
-		}
-		else{
-			set(value)
-		}
-		if(_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
-
-	}
-
-	/**
-	 * Watch for changes on this state
-	 * @param callback 
-	 * @returns 
-	*/
-	function watch(callback: PlexusStateWatcher<PxStateValue>): () => void
-	/**
-	 * Watch for changes on this state
-	 * @param keyOrCallback 
-	 * @param callback 
-	 * @returns 
-	*/
-	function watch(key: string | number, callback: PlexusStateWatcher<PxStateValue>): () => void
-	/**
-	 * Watch for changes on this state
-	 * @param keyOrCallback 
-	 * @param callback 
-	 * @returns 
-	*/
-	function watch(keyOrCallback: string | number | PlexusStateWatcher<PxStateValue>,  callback?: PlexusStateWatcher<PxStateValue>): () => void {
-		if(typeof keyOrCallback === 'function'){
-			callback = keyOrCallback
-			// generate a nonce from global instance
-			keyOrCallback = `_plexus_state_watcher_${instance().genNonce()}`
-		}
-
-		// add to internal list of named watchers
-		const destroy = instance()._runtime.subscribe(_internalStore._name, "stateChange", callback)
-		_internalStore._watchers.set(keyOrCallback, destroy)
-		// return keyOrCallback
-		return () => {
-			
-			removeWatcher(keyOrCallback as string | number)
-		}
-	}
-	/**
-	 * Remove a watcher from this state
-	 * @param key 
-	 * @returns 
-	*/
-	function removeWatcher(key: string | number){
-		// instance()._runtime.unsubscribe(_internalStore._name, key)
-		let destroy = _internalStore._watchers.get(key)
-		// if(!destroy) destroy = _internalStore._watchers.get(key.toString())
-		if(destroy) destroy()
-		return _internalStore._watchers.delete(key)
-
-	}
-	function removeAllWatchers(){
-		// instance()._runtime.unsubscribe(_internalStore._name, key)
-		_internalStore._watchers.forEach(destroy => {
-			if(destroy) destroy()
-		})
-		return _internalStore._watchers.clear()
-	}
-
-	/**
-	 * Persist the state to selected storage
-	 * @param name 
-	*/
-	function persist(name  : string ){
-		// if there is a name, change the states internal name 
-		if(name) _internalStore.externalName = `_plexus_state_${name}`
-
-		if(instance().storage){ 
-			instance().storage.set(_internalStore.externalName, _internalStore._value)
-			_internalStore._persist = true
-		}
-
-	}
-
-	/**
-	 * Reset the state to the previous value
-	*/
-	function undo(){
-		set(_internalStore._lastValue)
-	}
-
-	/**
-	 * Reset the state to the initial value
-	*/
-	function reset(){
-		set(_internalStore._initialValue)
-	}
+	
 	
 
-	const state = Object.freeze({
+	const state: PlexusStateInstance<StateValue> = Object.freeze({
 		/**
-		 * Set a new state value
-		 * @param value The new value of the state
+		 * Set the value of the state
+		 * @param value 
 		*/
-		set,
-		
-		patch,
+		set(value: StateValue) {
+
+			_internalStore._lastValue = _internalStore._value
+			if(isObject(value) && isObject(_internalStore._value)) {
+				_internalStore._lastValue = deepClone(_internalStore._value)
+			}
+			else if(Array.isArray(value) && Array.isArray(_internalStore._value)) {
+				const obj = deepMerge(_internalStore._value, value)
+				_internalStore._lastValue = (Object.values(obj) as StateValue)
+			}
+			else{
+				_internalStore._lastValue = _internalStore._value
+			}
+			_internalStore._value = value
+			_internalStore._nextValue = value
+
+			// update the runtime conductor
+			instance()._runtime.stateChange(_internalStore._name, value)
+			if(_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
+		},
+		/**
+		 * Patch the current value of the state
+		 * @param value A value of the state to merge with the current value
+		*/
+		patch(value: StateValue) {
+			
+			if(isObject(value) && isObject(_internalStore._value)) {
+				this.set(deepMerge(_internalStore._value, value))
+			}
+			// if the deep merge is on an array type, we need to convert the merged objecct back to an array
+			else if(Array.isArray(value) && Array.isArray(_internalStore._value)) {
+				const obj = deepMerge(_internalStore._value, value)
+				this.set(Object.values(obj) as StateValue)
+			}
+			else{
+				this.set(value)
+			}
+			if(_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
+
+		},
 		/**
 		 * Watch for changes on this state
 		 * @param keyOrCallback 
 		 * @param callback 
 		 * @returns 
 		*/
-		watch,
+		watch(keyOrCallback: string | number | PlexusStateWatcher<StateValue>,  callback?: PlexusStateWatcher<StateValue>): () => void {
+			if(typeof keyOrCallback === 'function'){
+				callback = keyOrCallback
+				// generate a nonce from global instance
+				keyOrCallback = `_plexus_state_watcher_${instance().genNonce()}`
+			}
+
+			// add to internal list of named watchers
+			const destroy = instance()._runtime.subscribe(_internalStore._name, "stateChange", callback)
+			_internalStore._watchers.set(keyOrCallback, destroy)
+			// return keyOrCallback
+			return () => {
+				
+				this.removeWatcher(keyOrCallback as string | number)
+			}
+		},
 		/**
 		 * Remove a watcher from this state
 		 * @param key 
 		 * @returns 
 		*/
-		removeWatcher,
-		
-		undo,
-		
-		reset,
-		
-		persist,
+		removeWatcher(key: string | number){
+			// instance()._runtime.unsubscribe(_internalStore._name, key)
+			let destroy = _internalStore._watchers.get(key)
+			// if(!destroy) destroy = _internalStore._watchers.get(key.toString())
+			if(destroy) destroy()
+			return _internalStore._watchers.delete(key)
+
+		},
+		// removeAllWatchers(){
+		// 	// instance()._runtime.unsubscribe(_internalStore._name, key)
+		// 	_internalStore._watchers.forEach(destroy => {
+		// 		if(destroy) destroy()
+		// 	})
+		// 	return _internalStore._watchers.clear()
+		// },
+
+		/**
+		 * Persist the state to selected storage
+		 * @param name 
+		*/
+		persist(name  : string ){
+			// if there is a name, change the states internal name 
+			if(name) _internalStore.externalName = `_plexus_state_${name}`
+
+			if(instance().storage){ 
+				instance().storage.set(_internalStore.externalName, _internalStore._value)
+				_internalStore._persist = true
+			}
+
+		},
+
+		/**
+		 * Reset the state to the previous value
+		*/
+		undo(){
+			this.set(_internalStore._lastValue)
+		},
+
+		/**
+		 * Reset the state to the initial value
+		*/
+		reset(){
+			this.set(_internalStore._initialValue)
+		},
 		/**
 		 * The current (reactive) value of the state
 		 */
