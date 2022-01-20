0027a7f5c43b874052d63ce832a36ab43c19aa0f
Philippe Clesca <hello@philippec.me>
2022-01-20 14:03:36 -0500

    Added type safty to usePlexus

--- packages/plexus-react/src/usePlexus.ts
+++ packages/plexus-react/src/usePlexus.ts
@@ -1,15 +1,24 @@
-import {state} from '@plexusjs/core'
+import {PlexusStateInstance, state} from '@plexusjs/core'
 // import {  } from '@plexusjs/core/dist/interfaces';
 import {useEffect, useState} from 'react';
 
-export function usePlexus<State extends ReturnType<typeof state>>(deps: State)
-export function usePlexus<State extends ReturnType<typeof state>>(deps: State[])
-export function usePlexus<State extends ReturnType<typeof state>>(deps: State | State[]){
+export type PlexusValue<T> = T extends PlexusStateInstance<infer U> ? U : never;
+export type PlexusValueArray<T> = { [K in keyof T]: T[K] extends PlexusStateInstance<infer U> ? U : never };
+
+
+const normalizeDeps = (deps: PlexusStateInstance | PlexusStateInstance[]) => Array.isArray(deps) ? deps : [deps];
+
+export function usePlexus<Value extends PlexusStateInstance<any>>(deps: Value): PlexusValue<Value>
+
+export function usePlexus<Value extends PlexusStateInstance<any>[]>(deps: Value): PlexusValueArray<Value>
+
+export function usePlexus<Value extends PlexusStateInstance<any>[]>(deps: Value | [] | PlexusStateInstance): PlexusValue<Value> | PlexusValueArray<Value>{
+	const depsArr = normalizeDeps(deps) as PlexusValueArray<Value>
 	const [_, set] = useState(null)
 	useEffect(() => {
-		if(Array.isArray(deps)){
+		if(Array.isArray(depsArr)){
 			const depUnsubs: Set<() => void> = new Set()
-			for(let dep of deps){
+			for(let dep of depsArr){
 				const unsubscribe = dep.watch(set)
 				depUnsubs.add(unsubscribe)
 			}
@@ -23,9 +32,9 @@ export function usePlexus<State extends ReturnType<typeof state>>(deps: State | 
 
 	}, [])
 
-	if(!Array.isArray(deps)){
-		return deps.value
+	if(!Array.isArray(deps) && depsArr.length === 1){
+		return deps.value as PlexusValue<Value>
 	}
-	return deps.map(dep => dep.value)
+	return depsArr.map<Value>(dep => dep.value) as PlexusValueArray<Value>
 
 } 
