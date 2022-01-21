017068a175f4370defff2b4f43d21d2851b53b63
Philippe Clesca <hello@philippec.me>
2022-01-21 16:27:45 -0500

    Selector sortof working

--- packages/plexus-core/src/collection/selector.ts
+++ packages/plexus-core/src/collection/selector.ts
@@ -1,25 +1,37 @@
 import { PlexusInstance } from "../instance"
-
-export interface PlexusCollectionSelector<T>{
-	select(key: string | number)
-	get value(): T[]
+import { DataKey, PlexusDataInstance } from "./data"
+export type SelectorName = string
+export interface PlexusCollectionSelector<T extends {[key: string]: any}>{
+	/**
+	 * Select an item in the collection
+	 * @param key The key to select
+	 */
+	select(key: DataKey)
+	get value(): T | null
+	get data(): PlexusDataInstance<T> | null
 }
 
-export function _selector<ValueType>(instance: () => PlexusInstance, collectionId: string, ): PlexusCollectionSelector<ValueType>{
+export function _selector<ValueType extends {[key: string]: any}>(instance: () => PlexusInstance, collectionId: string, ): PlexusCollectionSelector<ValueType>{
 	const _internalStore = {
 		_selected: null,
 		_collectionId: collectionId,
 	}
-	function select(key: string | number){
-		_internalStore._selected = key
-	}
+	
 	return {
-		select,
+		select(key: DataKey){
+			_internalStore._selected = key
+		},
 		get value(){
 			if(_internalStore._selected !== null){
 				return null
 			}
 			return instance()._collections.get(_internalStore._collectionId).getItemValue(_internalStore._selected)
+		},
+		get data(){
+			if(_internalStore._selected !== null){
+				return null
+			}
+			return instance()._collections.get(_internalStore._collectionId).getItem(_internalStore._selected)
 		},
 
 	}
