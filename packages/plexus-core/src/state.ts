6221e07ab7a610635a6d99ff08aca1564c7a8eb3
Philippe Clesca <hello@philippec.me>
2022-02-10 18:14:34 -0500

    added nextValue to state

--- packages/plexus-core/src/state.ts
+++ packages/plexus-core/src/state.ts
@@ -1,65 +1,81 @@
-import { deepClone, deepMerge, isObject } from './helpers'
-import { PlexusInstance } from './instance'
+import { deepClone, deepMerge, isObject } from "./helpers"
+import { PlexusInstance } from "./instance"
 // import { PlexusInstance, PlexStateInternalStore, PlexusStateType, PlexusStateInstance, PlexusStateWatcher } from "./interfaces"
-export type PlexusStateType = Object | Array<unknown> | string | number | boolean | null | undefined 
-export type PlexusState = <PxStateValue=any>(instance: () => PlexusInstance, input: PxStateValue) => PlexusStateInstance<PxStateValue>
+export type PlexusStateType = Object | Array<unknown> | string | number | boolean | null | undefined
+export type PlexusState = <PxStateValue = any>(
+	instance: () => PlexusInstance,
+	input: PxStateValue
+) => PlexusStateInstance<PxStateValue>
 export type PlexusStateWatcher<V> = (value: V) => void
-export interface PlexusStateInstance<Value=any> {
+export interface PlexusStateInstance<Value = any> {
+	/**
+	 * The value of the state
+	 */
+	value: Value
+	/**
+	 * The previous (reactive) value of the state
+	 */
+	lastValue: Value
+	/**
+	 * The next value to apply to the state
+	 * This is normally managed internally, but you can use it to "prepare" the state before applying the value.
+	 * @example state.nextValue = { foo: "bar" };
+	 * state.set(); // The state will be { foo: "bar" }
+	 */
+	nextValue: Value
+	name: string
 	/**
 	 * Set the value of the state
 	 * @param value A value of the state to merge with the current value
-	*/
-	set(item: Value): void;
+	 */
+	set(value?: Value): void
 	/**
 	 * Patch the current value of the state
 	 * @param value A value of the state to merge with the current value
-	*/
-	patch(item: Value): void;
+	 */
+	patch(value: Value): void
 	/**
 	 * Watch for changes on this state
 	 * @param key (optional) The key to use to identify this watcher
 	 * @param callback The callback to run when the state changes
 	 * @returns The remove function to stop watching
-	*/
-	watch(callback: PlexusStateWatcher<Value>): () => void;
-	watch(keyOrCallback: string | number | PlexusStateWatcher<Value>,  callback?: PlexusStateWatcher<Value>): () => void;
+	 */
+	watch(callback: PlexusStateWatcher<Value>): () => void
+	watch(keyOrCallback: string | number | PlexusStateWatcher<Value>, callback?: PlexusStateWatcher<Value>): () => void
 	/**
 	 * Remove a watcher from this state
 	 * @param key The key used to create the watcher
 	 * @returns true if the watcher was removed, false otherwise
-	*/
-	removeWatcher(key: string|number): boolean
+	 */
+	removeWatcher(key: string | number): boolean
 	/**
 	 * Reset the state to the previous value
-	*/
-	undo(): void;
+	 */
+	undo(): void
 	/**
 	 * Reset the state to the initial value
-	*/
-	reset(): void;
+	 */
+	reset(): void
 	/**
 	 * Persist the state to selected storage
 	 * @param name The storage prefix to use
-	*/
-	persist(name: string): void;
+	 */
+	persist(name: string): void
 	/**
 	 * The current (reactive) value of the state
 	 */
 	/**
 	 * On a set interval, run a function to update the state
-	 * @param setterFunction The function used to update the state on the interval
+	 * @param setterFunction The function used to update the state on the interval; returns the new value
 	 * @param ms The interval duration (in milliseconds)
 	 */
 	interval(setterFunction: (value: Value) => Value, ms?: number): this
-	clearInterval(): this
-	value: Value;
 	/**
-	 * The previous (reactive) value of the state
+	 * Stop the state interval
 	 */
-	lastValue: Value;
-	name: string;
+	clearInterval(): this
 	watcherRemovers: any
-} 
+}
 // export type PlexusStateInstance<Value=any> = ReturnType<typeof _state>
 
 type DestroyFn = () => void
@@ -77,8 +93,7 @@ export interface PlexStateInternalStore<Value> {
 }
 
 export function _state<StateValue extends PlexusStateType>(instance: () => PlexusInstance, _init: StateValue) {
-
-	// props // 
+	// props //
 	const _internalStore: PlexStateInternalStore<StateValue> = {
 		_nextValue: null,
 		_value: _init,
@@ -88,56 +103,54 @@ export function _state<StateValue extends PlexusStateType>(instance: () => Plexu
 		_name: `_plexus_state_${instance().genNonce()}`,
 		_persist: false,
 		_interval: null,
-		externalName: '',
-
+		externalName: "",
 	}
-	
+
 	// Methods //
-	
-	
 
 	const state: PlexusStateInstance<StateValue> = Object.freeze({
-		
-		set(value: StateValue) {
-
+		set(value?: StateValue) {
 			_internalStore._lastValue = _internalStore._value
-			if(isObject(value) && isObject(_internalStore._value)) {
+			if (isObject(value) && isObject(_internalStore._value)) {
 				_internalStore._lastValue = deepClone(_internalStore._value)
-			}
-			else if(Array.isArray(value) && Array.isArray(_internalStore._value)) {
+			} else if (Array.isArray(value) && Array.isArray(_internalStore._value)) {
 				const obj = deepMerge(_internalStore._value, value)
-				_internalStore._lastValue = (Object.values(obj) as StateValue)
-			}
-			else{
+				_internalStore._lastValue = Object.values(obj) as StateValue
+			} else {
 				_internalStore._lastValue = _internalStore._value
 			}
-			_internalStore._value = value
-			_internalStore._nextValue = value
+			// apply the next value
+			if (value === undefined) {
+				_internalStore._value = _internalStore._nextValue
+			} else {
+				_internalStore._value = value
+			}
+			_internalStore._nextValue = deepClone(_internalStore._value)
 
 			// update the runtime conductor
 			instance()._runtime.stateChange(_internalStore._name, value)
-			if(_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
+			if (_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
 		},
-		
+
 		patch(value: StateValue) {
-			
-			if(isObject(value) && isObject(_internalStore._value)) {
+			if (isObject(value) && isObject(_internalStore._value)) {
 				this.set(deepMerge(_internalStore._value, value))
 			}
 			// if the deep merge is on an array type, we need to convert the merged objecct back to an array
-			else if(Array.isArray(value) && Array.isArray(_internalStore._value)) {
+			else if (Array.isArray(value) && Array.isArray(_internalStore._value)) {
 				const obj = deepMerge(_internalStore._value, value)
 				this.set(Object.values(obj) as StateValue)
-			}
-			else{
+			} else {
 				this.set(value)
 			}
-			if(_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
-
+			if (_internalStore._persist) instance().storage.set(_internalStore.externalName, _internalStore._value)
 		},
-		
-		watch(keyOrCallback: string | number | PlexusStateWatcher<StateValue>,  callback?: PlexusStateWatcher<StateValue>): () => void {
-			if(typeof keyOrCallback === 'function'){
+
+		watch(
+			keyOrCallback: string | number | PlexusStateWatcher<StateValue>,
+			callback?: PlexusStateWatcher<StateValue>
+		): () => void {
+			if (typeof keyOrCallback === "function") {
 				callback = keyOrCallback
 				// generate a nonce from global instance
 				keyOrCallback = `_plexus_state_watcher_${instance().genNonce()}`
@@ -148,18 +161,16 @@ export function _state<StateValue extends PlexusStateType>(instance: () => Plexu
 			_internalStore._watchers.set(keyOrCallback, destroy)
 			// return keyOrCallback
 			return () => {
-				
 				this.removeWatcher(keyOrCallback as string | number)
 			}
 		},
-		
-		removeWatcher(key: string | number){
+
+		removeWatcher(key: string | number) {
 			// instance()._runtime.unsubscribe(_internalStore._name, key)
 			let destroy = _internalStore._watchers.get(key)
 			// if(!destroy) destroy = _internalStore._watchers.get(key.toString())
-			if(destroy) destroy()
+			if (destroy) destroy()
 			return _internalStore._watchers.delete(key)
-
 		},
 		// removeAllWatchers(){
 		// 	// instance()._runtime.unsubscribe(_internalStore._name, key)
@@ -169,63 +180,64 @@ export function _state<StateValue extends PlexusStateType>(instance: () => Plexu
 		// 	return _internalStore._watchers.clear()
 		// },
 
-		
-		persist(name: string ){
-			// if there is a name, change the states internal name 
-			if(name) _internalStore.externalName = `_plexus_state_${name}`
+		persist(name: string) {
+			// if there is a name, change the states internal name
+			if (name) _internalStore.externalName = `_plexus_state_${name}`
 
-			if(instance().storage){ 
+			if (instance().storage) {
 				instance().storage.set(_internalStore.externalName, _internalStore._value)
 				_internalStore._persist = true
 			}
-
 		},
 
-		
-		undo(){
+		undo() {
 			this.set(_internalStore._lastValue)
 		},
 
-		
-		reset(){
+		reset() {
 			this.set(_internalStore._initialValue)
 		},
-		interval(intervalCallback: (value: StateValue) => StateValue, ms?: number){
-			if(_internalStore._interval) clearInterval(_internalStore._interval)
+		interval(intervalCallback: (value: StateValue) => StateValue, ms?: number) {
+			if (_internalStore._interval) clearInterval(_internalStore._interval)
 			_internalStore._interval = setInterval(() => {
 				this.set(intervalCallback(this.value))
 			}, ms ?? 3000)
 			return this
 		},
-		clearInterval(){
-			if(_internalStore._interval) clearInterval(_internalStore._interval)
+		clearInterval() {
+			if (_internalStore._interval) clearInterval(_internalStore._interval)
 			return this
 		},
-		
+
 		get value() {
 			return deepClone(_internalStore._value)
 		},
-		
-		get lastValue(){
+
+		get lastValue() {
 			return deepClone(_internalStore._lastValue)
 		},
-		get name(){
+		get name() {
 			return _internalStore._name
 		},
-		get watcherRemovers(){
+		get watcherRemovers() {
 			return instance()._runtime.getWatchers(_internalStore._name)
-		}
+		},
+		get nextValue() {
+			return _internalStore._nextValue
+		},
+		set nextValue(value: StateValue) {
+			_internalStore._nextValue = value
+		},
 	})
 
 	// initalization //
-	if (instance()._states.has(_internalStore._name+"")) {
-		instance()._states.delete(_internalStore._name+"")
+	if (instance()._states.has(_internalStore._name + "")) {
+		instance()._states.delete(_internalStore._name + "")
 	}
 	// instance()._states.forEach(state_ => {
 	// 	state_.name
 	// })
-	instance()._states.set(_internalStore._name+"", state)
-	
+	instance()._states.set(_internalStore._name + "", state)
 
 	return state
 }
--- packages/plexus-core/tests/state.test.ts
+++ packages/plexus-core/tests/state.test.ts
@@ -1,86 +1,96 @@
-import { PlexusStateInstance, state } from '../src'
+import { PlexusStateInstance, state } from "../src"
 // import { PlexusState, PlexusStateInstance } from '../src/interfaces';
 let booleanState: PlexusStateInstance<boolean>,
- stringState: PlexusStateInstance<string>,
- objectState: PlexusStateInstance<{ a?: Partial<{ a: boolean, b: boolean }>, b?: boolean, c?: {b?: boolean}}>,
- arrayState: PlexusStateInstance<{ item?: string, item2?: {subitem?: string}}[]>,
- nullState: PlexusStateInstance<null | boolean>
+	stringState: PlexusStateInstance<string>,
+	objectState: PlexusStateInstance<Partial<{ a: { a?: boolean; b?: boolean }; b: boolean; c: { b?: boolean } }>>,
+	arrayState: PlexusStateInstance<{ item?: string; item2?: { subitem?: string } }[]>,
+	nullState: PlexusStateInstance<null | boolean>
 
 const initialValue = {
 	boolean: true,
-	string: 'Hello Pulse!',
+	string: "Hello Pulse!",
 	object: { a: { a: true, b: true }, b: true },
-	array: [{ item: 'Hello', item2: { subitem: 'World' } }, { item: 'Goodbye', item2: { subitem: 'People' } }],
-	null: null
+	array: [
+		{ item: "Hello", item2: { subitem: "World" } },
+		{ item: "Goodbye", item2: { subitem: "People" } },
+	],
+	null: null,
 }
 
 beforeEach(() => {
-	booleanState = state(initialValue.boolean);
-	stringState = state(initialValue.string);
-	objectState = state<{ a?: { a?: boolean, b?: boolean }, b?: boolean, c?: {b?: boolean}}>(initialValue.object);
-	arrayState = state<{ item?: string, item2?: {subitem?: string}}[]>(initialValue.array);
-	nullState = state(initialValue.null);
+	booleanState = state(initialValue.boolean)
+	stringState = state(initialValue.string)
+	objectState = state<{ a?: { a?: boolean; b?: boolean }; b?: boolean; c?: { b?: boolean } }>(initialValue.object)
+	arrayState = state<{ item?: string; item2?: { subitem?: string } }[]>(initialValue.array)
+	nullState = state(initialValue.null)
 })
-describe('Testing State Function', () => {
-	test('can save a value', () => {
+describe("Testing State Function", () => {
+	test("Can save a value", () => {
 		const value = state(1)
 		expect(value.value).toBe(1)
 	})
-	
-	test('Change value and remember the old one', () => { 
+
+	test("Change value and remember the old one", () => {
 		const value = state(1)
 		value.set(2)
 		expect(value.value).toBe(2)
 		expect(value.lastValue).toBe(1)
-	});
-	
-	test('Checking state().set()', () => { 
-		// check .set(value: object)
-		objectState.set({ a: { b: false }});
-		// check if the object is actually merged and children props do get overwritten
-		expect(objectState.value.a.b).toBe(false)
-		
-		
 	})
 
-	test('Checking state().patch()', () => {
+	test("Checking state().set()", () => {
+		// check .set(value: object)
+		objectState.set({ a: { b: false } })
+		// check if the object is actually merged and children props do get overwritten
+		expect(objectState.value.a.b).toBe(false)
+	})
+
+	test("Checking state().patch()", () => {
 		// can the object deep merge?
-		objectState.patch({ a: { b: false }});
+		objectState.patch({ a: { b: false } })
 		expect(objectState.value.a.a).toBe(true)
 		// check that other value is still there
 		expect(objectState.value.b).toBe(true)
 		// changed intended value
 		expect(objectState.value.a.b).toBe(false)
-		
 
 		// console.log(arrayState.value)
 		// check array deep merge
-		arrayState.patch([{ item: 'Hello2' }]);
+		arrayState.patch([{ item: "Hello2" }])
 		// console.log(arrayState.value)
-		expect(arrayState.value[0].item).toBe('Hello2')
-		expect(arrayState.value[0].item2).toStrictEqual({ subitem: 'World' })
-		expect(arrayState.value[1].item).toBe('Goodbye')
-
+		expect(arrayState.value[0].item).toBe("Hello2")
+		expect(arrayState.value[0].item2).toStrictEqual({ subitem: "World" })
+		expect(arrayState.value[1].item).toBe("Goodbye")
 	})
 
-	test('Checking state.watch()', () => {
-		let callbackCalled = false;
+	test("Checking state.watch()", () => {
+		let callbackCalled = false
 		const callback = () => {
 			// console.log('callback called', stringState.watchers)
-			callbackCalled = !callbackCalled;
+			callbackCalled = !callbackCalled
 		}
 		// can add watcher
-		const watcherDestoryer = stringState.watch(callback);
-		stringState.set('Hello World');
-		expect(callbackCalled).toBe(true);
+		const watcherDestoryer = stringState.watch(callback)
+		stringState.set("Hello World")
+		expect(callbackCalled).toBe(true)
 		// console.log(_instance()._runtime.getWatchers())
 		// can remove watcher
 		// stringState.removeWatcher(watcherKey);
 		watcherDestoryer()
 		// console.log(_instance()._runtime.getWatchers())
-		stringState.set('new value');
-		expect(callbackCalled).toBe(true);
-		
-		
+		stringState.set("new value")
+		expect(callbackCalled).toBe(true)
+	})
+
+	test("Checking state nextValue", () => {
+		// check .set(value: object)
+		objectState.set({ a: { b: false } })
+		objectState.nextValue = { a: { b: true } }
+
+		// check if the object is actually merged and children props do get overwritten
+		expect(objectState.value.a.b).toBe(false)
+		objectState.set()
+		expect(objectState.value.a.b).toBe(true)
+		console.log(objectState.value, objectState.nextValue)
+		expect(objectState.nextValue).toStrictEqual(objectState.value)
 	})
 })
