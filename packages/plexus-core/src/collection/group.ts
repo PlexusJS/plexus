41a83f889252970b9f56ae53a1a8eee0cee2fb6f
Philippe Clesca <hello@philippec.me>
2022-02-12 10:35:08 -0500

    made group watchable

--- packages/plexus-core/src/collection/group.ts
+++ packages/plexus-core/src/collection/group.ts
@@ -1,6 +1,7 @@
-import { PlexusCollectionInstance } from "..";
-import { PlexusInstance } from "../instance";
-import { DataKey, PlexusDataInstance } from "./data";
+import { PlexusCollectionInstance } from ".."
+import { PlexusInstance } from "../instance"
+import { PlexusStateWatcher } from "../state"
+import { DataKey, PlexusDataInstance } from "./data"
 
 export interface PlexusCollectionGroupConfig<DataType> {
 	addWhen?: (item: DataType) => boolean
@@ -11,17 +12,23 @@ export interface PlexusCollectionGroup<DataType> {
 	 * Check if the group contains the given item
 	 * @param key The key of the item to look for
 	 */
-	has( key: DataKey ): boolean,
+	has(key: DataKey): boolean
 	/**
 	 * Add an item to the group
 	 * @param key The key of the item to look for
 	 */
-	add( key: DataKey ): PlexusCollectionGroup<DataType>,
+	add(key: DataKey): PlexusCollectionGroup<DataType>
 	/**
 	 * Remove an item from the group
 	 * @param key The key of the item to look for
 	 */
-	remove( key: DataKey ): PlexusCollectionGroup<DataType>,
+	remove(key: DataKey): PlexusCollectionGroup<DataType>
+	/**
+	 * Watch for changes on this group
+	 * @param callback The callback to run when the state changes
+	 * @returns The remove function to stop watching
+	 */
+	watch(callback: PlexusStateWatcher<DataType>): () => void
 	/**
 	 * Peek at the index of the group (get all of the lookup keys for the group)
 	 */
@@ -35,34 +42,48 @@ export interface PlexusCollectionGroup<DataType> {
 	 */
 	get data(): PlexusDataInstance<DataType>[]
 }
-export function _group<DataType=any>(instance: () => PlexusInstance, collectionId: string, name: string, config?: PlexusCollectionGroupConfig<DataType>){
+export function _group<DataType = any>(
+	instance: () => PlexusInstance,
+	collectionId: string,
+	name: string,
+	config?: PlexusCollectionGroupConfig<DataType>
+) {
 	const _internalStore = {
 		addWhen: config?.addWhen || (() => false),
 		_name: name,
 		_collectionId: collectionId,
-		_includedKeys: new Set<string|number>()
+		_includedKeys: new Set<string | number>(),
 	}
 
-	return {
-		has( key: DataKey ){
+	return Object.freeze({
+		has(key: DataKey) {
 			return _internalStore._includedKeys.has(key)
 		},
-		add( key: DataKey ){
+		add(key: DataKey) {
 			_internalStore._includedKeys.add(key)
 			return this as PlexusCollectionGroup<DataType>
 		},
-		remove( key: DataKey ){
+		remove(key: DataKey) {
 			_internalStore._includedKeys.delete(key)
 			return this as PlexusCollectionGroup<DataType>
 		},
-		get index(){
+		get index() {
 			return _internalStore._includedKeys
-		},	
-		get value(){
+		},
+		get value() {
 			return instance()._collections.get(_internalStore._collectionId).groupsValue[_internalStore._name]
 		},
-		get data(){
-			return  Array.from(_internalStore._includedKeys).map(key => instance()._collections.get(_internalStore._collectionId).getItem(key)) 
-		}
-	}
+		get data() {
+			return Array.from(_internalStore._includedKeys).map((key) =>
+				instance()._collections.get(_internalStore._collectionId).getItem(key)
+			)
+		},
+		watch(callback?: PlexusStateWatcher<DataType>) {
+			const destroyers = this.data.map((data) => data.watch(callback))
+			const destroyer = () => {
+				destroyers.forEach((destroyer) => destroyer())
+			}
+			return destroyer
+		},
+	})
 }
