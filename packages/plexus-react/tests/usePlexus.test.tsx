2cfe07e475366ce7f5d3b414c75f23687aeff752
Philippe Clesca <hello@philippec.me>
2022-02-13 20:08:30 -0500

    Added a collection check on usePlexus testcases

--- packages/plexus-react/tests/usePlexus.test.tsx
+++ packages/plexus-react/tests/usePlexus.test.tsx
@@ -1,22 +1,28 @@
-import * as React from 'react';
-import {usePlexus} from '../dist';
-import * as renderer from 'react-test-renderer';
-import { PlexusStateInstance, state } from '@plexusjs/core';
+import * as React from "react"
+import { usePlexus } from "../dist"
+import * as renderer from "react-test-renderer"
+import { collection, PlexusStateInstance, state } from "@plexusjs/core"
 
 let myState: PlexusStateInstance
+const myCollection = collection<{ text: string }>().createGroup("group1")
 beforeEach(() => {
-	myState = state('yes')
+	myState = state("yes")
+	myCollection.collect({ text: "hello" }, "group1")
 })
 
-describe('Test react integration', () => {
-	test('usePlexus hook', () => {
+describe("Test react integration", () => {
+	test("usePlexus hook", () => {
 		function RandomComponent() {
 			const stateValue = usePlexus(myState)
-			return <div>
-				<p>{stateValue}</p>
-			</div>
+			const g1 = usePlexus(myCollection.getGroup("group1"))
+			
+			return (
+				<div>
+					<p>{stateValue}</p>
+				</div>
+			)
 		}
-		const tree = renderer.create(<RandomComponent />).toJSON();
-		expect(tree).toMatchSnapshot();
+		const tree = renderer.create(<RandomComponent />).toJSON()
+		expect(tree).toMatchSnapshot()
 	})
 })
