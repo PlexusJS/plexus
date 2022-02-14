15a7aa51eb61589fa285cb79b1b8b1f963e1f0ab
Nicolas Theck <contact@itsrems.com>
2022-02-14 01:57:26 +0100

    usePlexus groups support

--- packages/plexus-react/src/usePlexus.ts
+++ packages/plexus-react/src/usePlexus.ts
@@ -2,17 +2,21 @@ import { PlexusCollectionGroup, PlexusStateInstance, state } from '@plexusjs/cor
 // import {  } from '@plexusjs/core/dist/interfaces';
 import { useEffect, useState } from 'react';
 
-export type PlexusValue<T> = T extends PlexusStateInstance<infer U> ? U : never;
-export type PlexusValueArray<T> = { [ K in keyof T ]: T[ K ] extends PlexusStateInstance<infer U> ? U : never };
+export type PlexusValue<T> = T extends PlexusStateInstance<infer U> ? U : T extends PlexusCollectionGroup<infer U> ? U[] : never;
+export type PlexusValueArray<T> = {
+	[K in keyof T]: T[K] extends PlexusStateInstance<infer U> ? U
+	: T[K] extends PlexusCollectionGroup<infer U> ? U[]
+	: never
+};
 
 
 const normalizeDeps = (deps: PlexusStateInstance | PlexusStateInstance[] | PlexusCollectionGroup | PlexusCollectionGroup[]) => Array.isArray(deps) ? deps : [ deps ];
 
-export function usePlexus<Value extends PlexusStateInstance<any>>(deps: Value): PlexusValue<Value>
+export function usePlexus<Value extends PlexusStateInstance<any>|PlexusCollectionGroup<any>>(deps: Value): PlexusValue<Value>
 
-export function usePlexus<Value extends PlexusStateInstance<any>[]>(deps: Value): PlexusValueArray<Value>
+export function usePlexus<Value extends PlexusStateInstance<any>[]|PlexusCollectionGroup<any>[]>(deps: Value): PlexusValueArray<Value>
 
-export function usePlexus<Value extends PlexusStateInstance<any>[]>(deps: Value | [] | PlexusStateInstance | PlexusCollectionGroup): PlexusValue<Value> | PlexusValueArray<Value> {
+export function usePlexus<Value extends PlexusStateInstance<any>[]|PlexusCollectionGroup<any>[]>(deps: Value | [] | PlexusStateInstance | PlexusCollectionGroup): PlexusValue<Value> | PlexusValueArray<Value> {
 	const depsArr = normalizeDeps(deps) as PlexusValueArray<Value>
 	const [ _, set ] = useState(null)
 	useEffect(() => {
@@ -37,6 +41,7 @@ export function usePlexus<Value extends PlexusStateInstance<any>[]>(deps: Value 
 	if (!Array.isArray(deps) && depsArr.length === 1) {
 		return deps.value as PlexusValue<Value>
 	}
+	// @ts-ignore
 	return depsArr.map<Value>(dep => dep.value) as PlexusValueArray<Value>
 
-} 
+}
--- packages/plexus-react/tests/usePlexus.test.tsx
+++ packages/plexus-react/tests/usePlexus.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {usePlexus} from '../dist';
 import * as renderer from 'react-test-renderer';
-import { PlexusStateInstance, state } from '@plexusjs/core';
+import { collection, PlexusStateInstance, state } from '@plexusjs/core';
 
 let myState: PlexusStateInstance
 beforeEach(() => {
@@ -9,11 +9,23 @@ beforeEach(() => {
 })
 
 describe('Test react integration', () => {
-	test('usePlexus hook', () => {
+	test('usePlexus hook w/state', () => {
 		function RandomComponent() {
 			const stateValue = usePlexus(myState)
 			return <div>
 				<p>{stateValue}</p>
+			</div>
+		}
+		const tree = renderer.create(<RandomComponent />).toJSON();
+		expect(tree).toMatchSnapshot();
+	})
+	test('usePlexus hook w/collection group', () => {
+		function RandomComponent() {
+			const col = collection().createGroup('test');
+			col.collect({ id: 'pog', a: 1 }, 'test');
+			const groupValue = usePlexus(col.groups.test);
+			return <div>
+				<p>{JSON.stringify(groupValue)}</p>
 			</div>
 		}
 		const tree = renderer.create(<RandomComponent />).toJSON();
