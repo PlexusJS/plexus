c79468d9cee729b96e946d5fcd609202f55251ea
Philippe Clesca <hello@philippec.me>
2022-02-13 20:17:22 -0500

    Added watch function to selectors

--- packages/plexus-core/src/collection/selector.ts
+++ packages/plexus-core/src/collection/selector.ts
@@ -1,50 +1,65 @@
 import { PlexusInstance } from "../instance"
+import { PlexusStateWatcher } from "../state"
 import { DataKey, PlexusDataInstance } from "./data"
 export type SelectorName = string
-export interface PlexusCollectionSelector<T extends {[key: string]: any}>{
+export interface PlexusCollectionSelector<ValueType extends { [key: string]: any }> {
 	/**
 	 * The key of a data item assigned to this selecor
 	 */
-	get key(): DataKey 
+	get key(): DataKey
 	/**
 	 * Select an item in the collection
 	 * @param key The key to select
 	 */
 	select(key: DataKey)
 	/**
+	 * Watch for changes on this group
+	 * @param callback The callback to run when the state changes
+	 * @returns The remove function to stop watching
+	 */
+	watch(callback: PlexusStateWatcher<ValueType>): () => void
+	/**
 	 * Return the data value of the selected item
 	 */
-	get value(): T | null
+	get value(): ValueType | null
 	/**
 	 * The data of the selector
 	 */
-	get data(): PlexusDataInstance<T> | null
+	get data(): PlexusDataInstance<ValueType> | null
 }
 
-export function _selector<ValueType extends {[key: string]: any}>(instance: () => PlexusInstance, collectionId: string, name: string): PlexusCollectionSelector<ValueType>{
+export function _selector<ValueType extends { [key: string]: any }>(
+	instance: () => PlexusInstance,
+	collectionId: string,
+	name: string
+): PlexusCollectionSelector<ValueType> {
 	const _internalStore = {
 		_name: name,
 		_key: null,
 		_collectionId: collectionId,
 	}
-	
+
 	return {
-		get key(){ return _internalStore._key},
-		select(key: DataKey){
+		get key() {
+			return _internalStore._key
+		},
+		select(key: DataKey) {
 			_internalStore._key = key
 		},
-		get value(){
-			if(_internalStore._key === null){
+		get value() {
+			if (_internalStore._key === null) {
 				return null
 			}
 			return instance()._collections.get(_internalStore._collectionId).getItemValue(_internalStore._key)
 		},
-		get data(){
-			if(_internalStore._key === null){
+		get data() {
+			if (_internalStore._key === null) {
 				return null
 			}
 			return instance()._collections.get(_internalStore._collectionId).getItem(_internalStore._key)
 		},
-
+		watch(callback: PlexusStateWatcher<ValueType>) {
+			return this.data.watch(callback)
+		},
 	}
 }
