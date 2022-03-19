5290be878aa5a85ded0e32133a82996d086cab46
Philippe Clesca <hello@philippec.me>
2022-03-19 10:35:13 -0400

    improvments to interfaces

--- packages/plexus-core/src/interfaces.ts
+++ packages/plexus-core/src/interfaces.ts
@@ -3,15 +3,15 @@ import { _runtime } from "./runtime"
 
 export type PlexusRuntime = ReturnType<typeof _runtime>
 export type PlexusEvent = ReturnType<typeof _event>
-export type PlexusWatcher<V> = (value: V) => void
+export type PlexusWatcher<V extends any = any> = (value: V) => void
 
 export type Watchable = { [key: string | number]: any } & {
-	watch<V>(callback: PlexusWatcher<V>): () => void
+	watch<V extends any = any>(callback: PlexusWatcher<V>): () => void
 	name?: string
-	key?: string | ((key: string) => unknown)
+	key?: string | number | ((key: string) => unknown)
 }
 
-export type WatchableValue<V = any> = Watchable & { value: V; set(value: V): void }
+export type WatchableValue<V extends any = any> = Watchable & { value: V } & ({ set(value: V): void } | { select(value: V): void })
 
 export const isWatchable = (value: any): value is Watchable => {
 	return value && typeof value.watch === "function"
