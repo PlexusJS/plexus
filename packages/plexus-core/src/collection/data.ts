53ce3a77c6d85c5aa30702eca2d8d4060f6f8699
Philippe Clesca <hello@philippec.me>
2022-04-27 13:44:23 -0400

    DATA: Updated to the new retuend class syntax

--- packages/plexus-core/src/collection/data.ts
+++ packages/plexus-core/src/collection/data.ts
@@ -1,77 +1,106 @@
 import { PlexusStateInstance, state } from ".."
 import { PlexusInstance } from "../instance"
 import { PlexusWatcher } from "../interfaces"
+import { StateInstance } from "../state"
+interface PlexusDataStore<ValueType extends Record<string, any>> {
+	_key: string
+	primaryKey: string
+	_state: StateInstance<ValueType>
+}
 
-export interface PlexusDataInstance<TypeValue> {
+export type PlexusDataInstance<ValueType extends Record<string, any> = Record<string, any>> = CollectionDataInstance<ValueType>
+export type DataKey = string | number
+
+type DataObjectType<PK extends string = "id"> = Record<string, any> & { [Key in PK]: DataKey }
+export class CollectionDataInstance<ValueType extends DataObjectType<PK> = any, PK extends string = string> {
+	private instance: () => PlexusInstance
+	primaryKey: PK
+	private _internalStore: PlexusDataStore<ValueType>
+	constructor(instance: () => PlexusInstance, primaryKey: PK, value: ValueType) {
+		this.instance = instance
+		this.primaryKey = primaryKey
+		this._internalStore = {
+			_key: value[primaryKey].toString(),
+			primaryKey,
+			_state: state<ValueType>(value).key(`collection_data_${value[primaryKey]}`),
+		}
+		// this.value = value
+	}
 	/**
 	 * Get the value of the data instance
 	 */
-	get value(): TypeValue
+	get value() {
+		return this._internalStore._state.value
+	}
 	/**
 	 * Set the value of the data instance
 	 * @param value The value to set
 	 * @param config The config to use when setting the value
 	 * @param config.mode should we 'patch' or 'replace' the value
 	 */
-	set(value: TypeValue, config?: { mode: "replace" | "patch" }): void
+	set(value: Partial<ValueType>, config: { mode: "replace" | "patch" } = { mode: "replace" }) {
+		const checkIfHasKey = () => {
+			const v = value[this._internalStore.primaryKey as PK]
+			const valid =
+				value[this._internalStore.primaryKey] !== undefined &&
+				value[this._internalStore.primaryKey as PK].toString() === this._internalStore._key.toString()
+			this.instance().runtime.log(
+				"warn",
+				`The new data value ${valid ? "WILL" : "WILL NOT"} be set in "${config.mode}" mode`,
+				value,
+				value[this._internalStore.primaryKey],
+				this._internalStore._key,
+				value[this._internalStore.primaryKey] === this._internalStore._key
+			)
+			return valid
+		}
+		if (config.mode === "replace") {
+			if (checkIfHasKey()) {
+				this._internalStore._state.set(value as ValueType)
+			}
+		} else {
+			if (checkIfHasKey()) {
+				this._internalStore._state.patch(value as ValueType)
+			}
+		}
+	}
 	/**
 	 * The state that powers this data instance
 	 */
-	get state(): PlexusStateInstance<TypeValue>
+	get state() {
+		return this._internalStore._state
+	}
 	/**
 	 * Delete the data instance
 	 */
-	delete(): void
+	delete() {
+		this.instance().runtime.removeWatchers("state", this._internalStore._state.name)
+		this.instance()._states.delete(this._internalStore._state)
+		// delete _internalStore._state
+	}
 	/**
 	 * Watch for changes on this data instance
 	 * @param callback The callback to run when the state changes
 	 * @returns The remove function to stop watching
 	 */
-	watch(callback: PlexusWatcher<TypeValue>): () => void
+	watch(callback: PlexusWatcher<ValueType>) {
+		return this._internalStore._state.watch(callback)
+	}
 }
 
-export type DataKey = string | number
-
-export function _data<Value extends Record<string, any>>(
+export function _data<ValueType extends Record<string, any>>(
 	instance: () => PlexusInstance,
 	primaryKey: string,
-	value: Value
-): PlexusDataInstance<Value> | null {
-	const _internalStore = {
-		_key: value[primaryKey],
-		primaryKey,
-		_state: state<Value>(value).key(`collection_data_${value[primaryKey]}`),
-	}
+	value: ValueType
+): PlexusDataInstance<ValueType> | null {
+	// const _internalStore = {
+	// 	_key: value[primaryKey],
+	// 	primaryKey,
+	// 	_state: state<Value>(value).key(`collection_data_${value[primaryKey]}`),
+	// }
 
 	if (value[primaryKey] !== undefined && value[primaryKey] !== null) {
-		return {
-			get value() {
-				return _internalStore._state.value
-			},
-			set(value: Partial<Value>, config: { mode: "replace" | "patch" } = { mode: "replace" }) {
-				const checkIfHasKey = () => value[_internalStore.primaryKey] !== undefined && value[_internalStore.primaryKey] === _internalStore._key
-				if (config.mode === "replace") {
-					if (checkIfHasKey()) {
-						_internalStore._state.set(value as Value)
-					}
-				} else {
-					if (value[_internalStore.primaryKey] === _internalStore._key) {
-						_internalStore._state.patch(value as Value)
-					}
-				}
-			},
-			get state() {
-				return _internalStore._state
-			},
-			delete() {
-				instance()._runtime.removeWatchers("state", _internalStore._state.name)
-				instance()._states.delete(_internalStore._state)
-				// delete _internalStore._state
-			},
-			watch(callback: PlexusWatcher<Value>) {
-				return _internalStore._state.watch(callback)
-			},
-		}
+		return new CollectionDataInstance(instance, primaryKey, value)
 	}
 	return null
 }
