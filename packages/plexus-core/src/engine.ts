292677e2802e4dece502fc5adc44c5d544747e09
Philippe Clesca <hello@philippec.me>
2022-04-28 13:14:24 -0400

    ENGINE: Fix for failed removal of listeners + added testcases for engine

--- packages/plexus-core/src/engine.ts
+++ packages/plexus-core/src/engine.ts
@@ -10,10 +10,13 @@ export class EventEngine {
 	}
 	on(eventId: string | number, listener: PlexusWatcher, origin?: string) {
 		if (!eventId || eventId === "") {
+			console.warn("Event Engine: Missing an eventId")
 			return () => null
 		}
+		// create the variables to assign in the events list map
 		const from = origin || "unknown"
 		const eventWatcher = { from, listener }
+		// ensure the eventId has a list, if not, create it
 		if (!this.events.has(eventId)) {
 			this.events.set(eventId, [])
 		}
@@ -25,9 +28,12 @@ export class EventEngine {
 		if (!this.events.has(eventId)) {
 			return
 		}
-		const idx = this.events.get(eventId)?.indexOf(eventWatcher)
+		const idx = this.events.get(eventId)?.indexOf(eventWatcher) ?? -1
 		const eventWatchers = this.events.get(eventId)
-		if (eventWatchers && idx && idx > -1) {
+		// console.log(`found index ${idx}; with ${eventWatchers?.[0].listener}`)
+		if (eventWatchers && idx === 0) {
+			this.events.set(eventId, [...eventWatchers.splice(idx + 1)])
+		} else if (eventWatchers && idx > -1) {
 			this.events.set(eventId, [...eventWatchers.splice(0, idx - 1), ...eventWatchers.splice(idx + 1)])
 		}
 		if (this.events.get(eventId)?.length === 0) {
