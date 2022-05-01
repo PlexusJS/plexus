aca651c5a7faf255be0d0ef4d496b3a211aab0e9
Nicolas Theck <contact@itsrems.com>
2022-05-01 03:38:12 +0200

    DEBUG BUILD

--- packages/plexus-react/src/usePlexus.ts
+++ packages/plexus-react/src/usePlexus.ts
@@ -28,7 +28,8 @@ export function usePlexus<V extends WatchableValue[]>(deps: V | [] | WatchableVa
 			for (let dep of depsArr) {
 				// if not a watchable, then we can't watch it, skip to next iteration
 				if (!(dep instanceof WatchableValue)) continue
-				const unsubscribe = dep.watch(function () {
+				const unsubscribe = dep.watch(function (v) {
+					console.log('should updated', { v })
 					set({})
 				})
 				depUnsubs.add(unsubscribe)
