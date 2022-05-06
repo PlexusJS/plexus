ff33d97533da2f3a26cc2c5c8c7bd9077268c71b
Philippe Clesca <hello@philippec.me>
2022-05-06 03:54:27 -0400

    new fonts, better tailwind config, framer-motion, better categorization, better collapse component, Finished api docs for state finished api docs for api finished api docs for preaction

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,28 +1,49 @@
 import { type } from "os"
-import React from "react"
+import React, { ReactNode } from "react"
 type ListData = {
 	key?: string
 	type?: string
 	optional?: boolean
-	description: string
+	description: ReactNode
 }
-export const CollapseContainer = ({ title = "", data = [] }: { title: string; data: ListData[] }) => {
+interface Props {
+	title: string
+	data: ListData[]
+	miniTitle?: string
+	showingReturnType?: boolean
+}
+export const CollapseContainer = ({ title, data = [], miniTitle, showingReturnType = false }: Props) => {
 	return (
-		<div className="rounded-t-lg rounded-b-md  overflow-hidden flex flex-col bg-slate-800">
-			<div className="rounded-md bg-slate-700 p-4 flex justify-between">
-				<h6>{title}</h6>
+		<>
+			{(miniTitle || showingReturnType) && (
+				<div className="pt-3 pb-2 flex justify-between">
+					<small>{showingReturnType ? "Returns" : miniTitle}</small>
+				</div>
+			)}
+			<div className="rounded-t-lg rounded-b-md  overflow-hidden flex flex-col bg-slate-800 mb-5">
+				{title && (
+					<div className="rounded-md bg-slate-700 p-4 flex justify-between">
+						<h6>{title}</h6>
+					</div>
+				)}
+				<div className="py-4 px-4">
+					<ul>
+						{data.map((item, index) => (
+							<li key={`c_${index}`} className="hover:bg-slate-600 rounded-md px-3 py-2 transition-all duration-200">
+								<mark>
+									<span className="text-blue-700">
+										{item.key ? item.key : ""}
+										{item.optional ? "?" : ""}
+									</span>{" "}
+									{item.type && `{${item.type.trim()}}:`}
+								</mark>{" "}
+								{item.description}
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
-			<div className="py-4 px-4">
-				<ul>
-					{data.map((item, index) => (
-						<li className="hover:bg-slate-600 rounded-md px-3 py-2 transition-all duration-200">
-							{`${item.key ? item.key : ""}${item.optional ? "?" : ""}`} {item.type && <mark>{`{${item.type.trim()}}`}</mark>}:{" "}
-							{item.description}
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
+		</>
 	)
 }
 
