dd8d762ce0ba970002f158fac418b7eb418caf8a
Philippe Clesca <hello@philippec.me>
2022-05-08 04:31:34 -0400

    Try with new git url

--- packages/create-plexus-core/lib/postbuild.js
+++ packages/create-plexus-core/lib/postbuild.js
@@ -18,7 +18,7 @@ if (fs.existsSync(path)) {
 		console.log(chalk.green('bin.js file already has the correct script tag...'))
 	}
 	else {
-		console.log(chalk.red('bin.js file does not have the correct script tag...'))
+		console.log(chalk.yellow('bin.js file does not have the correct script tag...'))
 		console.log(chalk.green('Adding script tag to bin.js file...'))
 		fs.writeFileSync(path, upperScriptTag + '\n' + binContent)
 		console.log(chalk.green('bin.js file updated!'))
