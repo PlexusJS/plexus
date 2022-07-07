8451b6bd1379b8ea0d3969586572758127c14d44
Nicolas Theck <33380002+itsRems@users.noreply.github.com>
2022-07-07 11:06:50 +0200

    debug log

--- packages/plexus-next/src/index.ts
+++ packages/plexus-next/src/index.ts
@@ -76,9 +76,10 @@ export function loadServerState(plexus?: PlexusInstance, data: PlexusNextData = 
 
 			for (const collection of collections.values()) {
 				const fromSSR = data.collections[collection.name];
+				console.log({ fromSSR, collection });
 				if (fromSSR) {
 					if (fromSSR.data?.length > 0) collection.collect(fromSSR.data)
-					
+
 					if (fromSSR.groups) {
 						for (const gName in fromSSR.groups) {
 							const gKeys = fromSSR.groups[gName]
