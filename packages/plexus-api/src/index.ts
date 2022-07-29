d9f3d4008050074fb7bdeda38530101e075bc469
Philippe Clesca <hello@philippec.me>
2022-07-29 19:19:01 -0400

    INSTANCE: Added onReady method NEXT: Started mapping out new structure. (May not need to set a name explicitly)

--- packages/plexus-api/src/index.ts
+++ packages/plexus-api/src/index.ts
@@ -272,7 +272,7 @@ export class ApiInstance {
 	delete<ResponseType = any>(path: string) {
 		return this.preSend<ResponseType>(path, {
 			method: "DELETE",
-			body: JSON.stringify({})
+			body: JSON.stringify({}),
 		})
 	}
 	/**
