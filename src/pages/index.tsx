4e5d037a1ea2b0d31f141cf4c719e47d6384bb00
Philippe Clesca <hello@philippec.me>
2022-08-05 14:45:15 -0400

    Spacing fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ function HomepageHeader() {
 					</motion.svg>
 				</svg>
 				<div className=" h-72 flex justify-center">
-					Data Orchestration <mark> at it's Finest ✨</mark>. <i>Fully TypeSafe. Instantly Reactive.</i>
+					<mark>Data Orchestration at it's Finest ✨</mark>. <i>Fully TypeSafe. Instantly Reactive.</i>
 				</div>
 				<div className={styles.buttons}>
 					<Link className="button button--secondary button--lg" to="/docs">
