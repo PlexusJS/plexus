51e3476668f6f276642913b14e778c5b83e8d4bc
Philippe Clesca <hello@philippec.me>
2022-11-30 06:05:55 -0500

    Docs readability improvements

--- packages/plexus-core/src/collection/data.ts
+++ packages/plexus-core/src/collection/data.ts
@@ -5,248 +5,260 @@ import { PlexusCollectionInstance } from './collection'
 import { deepClone, deepMerge, isEqual } from '@plexusjs/utils'
 
 interface CollectionDataConfig {
-	prov: boolean
-	unfoundKeyIsUndefined?: boolean
+    prov: boolean
+    unfoundKeyIsUndefined?: boolean
 }
 interface PlexusDataStore<DataType extends Record<string, any>> {
-	primaryKey: string
-	_wDestroyers: Set<() => void>
-	_config: CollectionDataConfig
+    primaryKey: string
+    _wDestroyers: Set<() => void>
+    _config: CollectionDataConfig
 }
 
 export type PlexusDataInstance<
-	DataType extends Record<string, any> = Record<string, any>
+    DataType extends Record<string, any> = Record<string, any>
 > = CollectionDataInstance<DataType>
 export type DataKey = string | number
 
 // TODO: Remove the State Instance from the Data Instance's internalStore in favor of watchableValue's internalStore & logic
 type DataObjectType<PK extends string = 'id'> = Record<string, any> & {
-	[Key in PK]: DataKey
+    [Key in PK]: DataKey
 }
 
 /**
- * A collection of data
+ * A piece of data belonging to a collection
  */
 export class CollectionDataInstance<
-	DataType extends DataObjectType<PK> = any,
-	PK extends string = string
+    DataType extends DataObjectType<PK> = any,
+    PK extends string = string
 > extends WatchableMutable<DataType> {
-	private primaryKey: PK
-	readonly key: string | number
-	provisional: boolean
-	private _internalStore: PlexusDataStore<DataType>
+    private primaryKey: PK
+    readonly key: string | number
+    provisional: boolean
+    private _internalStore: PlexusDataStore<DataType>
 
-	constructor(
-		instance: () => PlexusInstance,
-		public collection: () => PlexusCollectionInstance<DataType>,
-		primaryKey: PK,
-		keyValue: string | number,
-		value: DataType,
-		config: CollectionDataConfig = { prov: false }
-	) {
-		super(instance, value)
-		this.provisional = config.prov
-		this.primaryKey = primaryKey
-		this.key = keyValue
+    constructor(
+        instance: () => PlexusInstance,
+        public collection: () => PlexusCollectionInstance<DataType>,
+        primaryKey: PK,
+        keyValue: string | number,
+        value: DataType,
+        config: CollectionDataConfig = { prov: false }
+    ) {
+        super(instance, value)
+        this.provisional = config.prov
+        this.primaryKey = primaryKey
+        this.key = keyValue
 
-		this._internalStore = {
-			primaryKey,
-			_wDestroyers: new Set<() => void>(),
-			_config: config,
-		}
-		if (!this.provisional) {
-			this.mount()
-		}
-	}
-	/**
-	 * The internal id of the state with an instance prefix
-	 */
-	get id(): string {
-		return `${this._watchableStore._internalId}`
-	}
-	/**
-	 * The internal id of the state with an instance prefix
-	 */
-	get instanceId(): string {
-		return `dat_${this._watchableStore._internalId}`
-	}
+        this._internalStore = {
+            primaryKey,
+            _wDestroyers: new Set<() => void>(),
+            _config: config,
+        }
+        if (!this.provisional) {
+            this.mount()
+        }
+    }
+    /**
+     * The internal id of the state with an instance prefix
+     * @type {string}
+     */
+    get id(): string {
+        return `${this._watchableStore._internalId}`
+    }
+    /**
+     * The internal id of the state with an instance prefix
+     * @type {string}
+     */
+    get instanceId(): string {
+        return `dat_${this._watchableStore._internalId}`
+    }
 
-	private mount() {
-		if (!this.instance()._collectionData.has(this)) {
-			this.instance()._collectionData.add(this)
-			this.instance().runtime.log(
-				'info',
-				`Hoisting collection data ${this.instanceId} with value`,
-				this._watchableStore._value,
-				`to instance`
-			)
-		}
-	}
+    private mount() {
+        if (!this.instance()._collectionData.has(this)) {
+            this.instance()._collectionData.add(this)
+            this.instance().runtime.log(
+                'info',
+                `Hoisting collection data ${this.instanceId} with value`,
+                this._watchableStore._value,
+                `to instance`
+            )
+        }
+    }
 
-	private checkIfHasKey(value?: Partial<DataType>): boolean {
-		// if the value is undefined/null
-		if (!value || !value[this._internalStore.primaryKey as PK]) {
-			return false
-		}
-		// Check if the value has the primary key, and verify the key is the same as the data instance
-		const isCurrentKey =
-			value[this._internalStore.primaryKey as PK].toString().trim() ===
-			this.key.toString().trim()
-		// if the key is not the same, then we can't use this value
-		const valid =
-			value[this._internalStore.primaryKey] !== undefined && isCurrentKey
-		this.instance().runtime.log(
-			'warn',
-			`The incoming value key does ${
-				valid ? '' : 'NOT'
-			} match the stored key...`,
-			this.key,
-			value[this._internalStore.primaryKey] === this.key
-		)
-		return valid
-	}
-	/**
-	 * Get the value of the data instance
-	 */
-	get value() {
-		const foreignKeys = this.collection().config.foreignKeys
-		if (foreignKeys) {
-			// type ForeignRecords = Record<
-			// 	keyof typeof foreignKeys[keyof typeof foreignKeys]["newKey"],
-			// 	ReturnType<typeof foreignKeys[keyof typeof foreignKeys]["reference"]>
-			// >
+    private checkIfHasKey(value?: Partial<DataType>): boolean {
+        // if the value is undefined/null
+        if (!value || !value[this._internalStore.primaryKey as PK]) {
+            return false
+        }
+        // Check if the value has the primary key, and verify the key is the same as the data instance
+        const isCurrentKey =
+            value[this._internalStore.primaryKey as PK].toString().trim() ===
+            this.key.toString().trim()
+        // if the key is not the same, then we can't use this value
+        const valid =
+            value[this._internalStore.primaryKey] !== undefined && isCurrentKey
+        this.instance().runtime.log(
+            'warn',
+            `The incoming value key does ${
+                valid ? '' : 'NOT'
+            } match the stored key...`,
+            this.key,
+            value[this._internalStore.primaryKey] === this.key
+        )
+        return valid
+    }
+    /**
+     * Get the value of the data instance
+     * @type {DataType}
+     */
+    get value() {
+        const foreignKeys = this.collection().config.foreignKeys
+        if (foreignKeys) {
+            // type ForeignRecords = Record<
+            // 	keyof typeof foreignKeys[keyof typeof foreignKeys]["newKey"],
+            // 	ReturnType<typeof foreignKeys[keyof typeof foreignKeys]["reference"]>
+            // >
 
-			const value = { ...super.value } as Partial<any> & DataType
-			let oldKey: keyof DataType
+            const value = { ...super.value } as Partial<any> & DataType
+            let oldKey: keyof DataType
 
-			for (oldKey of Object.keys(foreignKeys ?? {})) {
-				const newKey: keyof Partial<any> = foreignKeys[oldKey]
-					?.newKey as keyof Partial<any>
-				Object.defineProperty(value, newKey, {
-					get() {
-						return foreignKeys[oldKey]?.reference().value
-					},
-				})
-			}
+            for (oldKey of Object.keys(foreignKeys ?? {})) {
+                const newKey: keyof Partial<any> = foreignKeys[oldKey]
+                    ?.newKey as keyof Partial<any>
+                Object.defineProperty(value, newKey, {
+                    get() {
+                        return foreignKeys[oldKey]?.reference().value
+                    },
+                })
+            }
 
-			return value
-		}
-		return super.value
-	}
-	/**
-	 * The previous (reactive) value of the state
-	 */
-	get lastValue() {
-		return deepClone(this._watchableStore._lastValue)
-	}
-	/**
-	 * The initial (default) value of the state
-	 */
-	get initialValue() {
-		return deepClone(this._watchableStore._initialValue)
-	}
-	/**
-	 * Set the value of the data instance
-	 * @param value The value to set
-	 */
-	set(value?: Partial<DataType>) {
-		if (!value) return this
+            return value
+        }
+        return super.value
+    }
+    /**
+     * The previous (reactive) value of the state
+     * @type {DataType}
+     */
+    get lastValue() {
+        return deepClone(this._watchableStore._lastValue)
+    }
+    /**
+     * The initial (default) value of the state
+     * @type {DataType}
+     */
+    get initialValue() {
+        return deepClone(this._watchableStore._initialValue)
+    }
+    /**
+     * Set the value of the data instance
+     * @param {DataType} value The value to set
+     * @returns {this} The data instance
+     */
+    set(value?: Partial<DataType>): this {
+        if (!value) return this
 
-		// if this is provisional, mount to the collection & instance
-		if (this.provisional) {
-			this.instance().runtime.log(
-				'debug',
-				`Mounting provisional data instance "${this.key}" to instance...`
-			)
-			this.mount()
-			this.provisional = false
-		}
+        // if this is provisional, mount to the collection & instance
+        if (this.provisional) {
+            this.instance().runtime.log(
+                'debug',
+                `Mounting provisional data instance "${this.key}" to instance...`
+            )
+            this.mount()
+            this.provisional = false
+        }
 
-		if (!isEqual(value as DataType, this._watchableStore._value)) {
-			if (this.checkIfHasKey(value)) {
-				super.set(value as DataType)
-			} else {
-				// give the id to the new value if it's missing
-				super.set({ ...value, [this.primaryKey]: this.key } as DataType)
-			}
-		} else {
-			this.instance().runtime.log(
-				'warn',
-				`Tried applying the same value to data "${this.key}" in collection ${
-					this.collection().id
-				}...`
-			)
-		}
-		this.collection().lastUpdatedKey = this.key
+        if (!isEqual(value as DataType, this._watchableStore._value)) {
+            if (this.checkIfHasKey(value)) {
+                super.set(value as DataType)
+            } else {
+                // give the id to the new value if it's missing
+                super.set({ ...value, [this.primaryKey]: this.key } as DataType)
+            }
+        } else {
+            this.instance().runtime.log(
+                'warn',
+                `Tried applying the same value to data "${
+                    this.key
+                }" in collection ${this.collection().id}...`
+            )
+        }
+        this.collection().lastUpdatedKey = this.key
 
-		return this
-	}
-	/**
-	 * Patch the current value of the state
-	 * @param value A value of the state to merge with the current value
-	 */
-	patch(value: Partial<DataType>) {
-		this.set(deepMerge(this._watchableStore._value, value))
+        return this
+    }
+    /**
+     * Patch the current value of the state
+     * @param {DataType} value A value of the state to merge with the current value
+     * @returns {this} The data instance
+     */
+    patch(value: Partial<DataType>): this {
+        this.set(deepMerge(this._watchableStore._value, value))
 
-		this.collection().lastUpdatedKey = this.key
-		return this
-	}
+        this.collection().lastUpdatedKey = this.key
+        return this
+    }
 
-	/**
-	 * Compare a thing to the current value, if they are equal, returns true
-	 * @param value The thing to compare the current value to
-	 * @returns {boolean} A boolean representing if they are equal
-	 */
-	isEqual(value: any) {
-		return isEqual(value, super._watchableStore._value)
-	}
-	/**
-	 * Delete the data instance
-	 */
-	delete() {
-		this.collection().delete(this.key)
-
-		// delete _internalStore._state
-	}
-	/**
-	 * Clean this data instance (remove all watchers & remove the state from the instance)
-	 */
-	clean() {
-		this._internalStore._wDestroyers.forEach((destroyer) => destroyer())
-		this._internalStore._wDestroyers.clear()
-		this.instance()._collectionData.delete(this)
-	}
-	/**
-	 * Watch for changes on this data instance
-	 * @param callback The callback to run when the state changes
-	 * @returns The remove function to stop watching
-	 */
-	watch(callback: PlexusWatcher<DataType>, from?: string) {
-		const destroyer = super.watch(callback, from)
-		return destroyer
-	}
+    /**
+     * Compare a thing to the current value, if they are equal, returns true
+     * @param value The thing to compare the current value to
+     * @returns {boolean} A boolean representing if they are equal
+     */
+    isEqual(value: any): boolean {
+        return isEqual(value, super._watchableStore._value)
+    }
+    /**
+     * Delete the data instance
+     * @returns {this} The data instance
+     */
+    delete(): this {
+        this.collection().delete(this.key)
+        return this
+        // delete _internalStore._state
+    }
+    /**
+     * Clean this data instance (remove all watchers & remove the state from the instance)
+     * @returns {this} The data instance
+     */
+    clean(): this {
+        this._internalStore._wDestroyers.forEach((destroyer) => destroyer())
+        this._internalStore._wDestroyers.clear()
+        this.instance()._collectionData.delete(this)
+        return this
+    }
+    /**
+     * Watch for changes on this data instance
+     * @callback WatchCallback
+     * @callback killWatcher
+     * @param {WatchCallback} callback The callback to run when the state changes
+     * @returns {killWatcher} The remove function to stop watching
+     */
+    watch(callback: PlexusWatcher<DataType>, from?: string) {
+        const destroyer = super.watch(callback, from)
+        return destroyer
+    }
 }
 
 export function _data<DataType extends Record<string, any>>(
-	instance: () => PlexusInstance,
-	collection: () => PlexusCollectionInstance<DataType>,
-	primaryKey: string,
-	keyValue: number | string,
-	value: DataType,
-	config: CollectionDataConfig = { prov: false }
+    instance: () => PlexusInstance,
+    collection: () => PlexusCollectionInstance<DataType>,
+    primaryKey: string,
+    keyValue: number | string,
+    value: DataType,
+    config: CollectionDataConfig = { prov: false }
 ) {
-	if (
-		(value?.[primaryKey] !== undefined && value?.[primaryKey] !== null) ||
-		config.prov
-	) {
-		return new CollectionDataInstance(
-			instance,
-			collection,
-			primaryKey,
-			keyValue,
-			value,
-			config
-		)
-	}
-	return null
+    if (
+        (value?.[primaryKey] !== undefined && value?.[primaryKey] !== null) ||
+        config.prov
+    ) {
+        return new CollectionDataInstance(
+            instance,
+            collection,
+            primaryKey,
+            keyValue,
+            value,
+            config
+        )
+    }
+    return null
 }
