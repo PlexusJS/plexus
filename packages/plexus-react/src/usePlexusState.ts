2130c185ce4bae00072902e337cbcc6f6aeb0b7c
Philippe Clesca <hello@philippec.me>
2023-01-05 01:47:05 -0500

    fix usePlexusState setState type

--- packages/plexus-react/src/usePlexusState.ts
+++ packages/plexus-react/src/usePlexusState.ts
@@ -1,5 +1,6 @@
 import { WatchableValue } from '@plexusjs/core'
 import { PlexusValue, usePlexus } from './usePlexus'
+
 /**
  * A React hook to extract the values from plexus objects and reactively update the component and value when the value changes. This hook returns an array, similar to that of React's useState. The arguments are similar to useState as well, it returns the current value and a function to update the value.
  * @param dep The Plexus dependency to watch
@@ -7,7 +8,7 @@ import { PlexusValue, usePlexus } from './usePlexus'
  */
 export function usePlexusState<V extends WatchableValue>(
 	dep: V
-): [PlexusValue<V>, (value: V) => void] {
+): [PlexusValue<V>, (value: PlexusValue<V>) => void] {
 	const value = usePlexus(dep)
 	return [value, (nextValue) => dep.set(nextValue)]
 }
